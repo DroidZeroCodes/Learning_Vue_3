7074194d641d9dadb8a83721a6b145c6299c21c4
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-12-02 00:28:10 +0800

    Event handling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,86 @@
 <template>
-  <div>
+  <h1>{{ message }}</h1>
+  <button v-on:click="replaceText('v-on is fun')">Replace Text</button>
+  <button @click="replaceText('v-on is fun')">Replace Text</button>
+  <hr>
+  <p>Input field for numeric values only</p>
+  <input type="text" @keydown="handleInput($event)">
 
+  <hr>
+  <div style="width: 100px; height: 100px; background-color: aqua;" @contextmenu.prevent="console.log('Show custom context menu')"></div>
+  <div id="mouseover" @mouseover="fun1">
+    <textarea @mouseover.stop="fun2($event)">This is a text area.</textarea>
+  </div>
+
+   <hr />
+  <div>
+    Press down the "Enter" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.enter="console.log('You pressed the Enter key.')"
+    />
+  </div>
+  <div>
+    Press down the "Arrow Down" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.down="console.log('You pressed the Arrow Down key.')"
+    />
+  </div>
+  <div>
+    Press down the "Space" key will trigger a console log print:
+    <input
+      type="text"
+      @keydown.space="console.log('You pressed the Space key.')"
+    />
+  </div>
+  <div>
+    Press down the "b" key will trigger a console log print:
+    <input type="text" @keydown.b="console.log('You pressed the B key.')" />
+  </div>
+  <div>
+    Press down the "Ctrl c" will trigger a console log print:
+    <input type="text" @keydown.ctrl.c="console.log('You pressed Ctrl c.')" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+
+let message = ref('Hello, v-on!');
+
+function replaceText(newText) {
+  message.value = newText;
+}
+
+function handleInput(event) {
+  console.log(event);
+  let keyCode = event.keyCode
+  if (keyCode === 8 || keyCode === 13) {
+    return; // Allow backspace and enter keys
+  }
+  if(keyCode < 48 || keyCode > 57) {
+    event.preventDefault();
+  }
+}
+
+function fun1() {
+  console.log('Mouse over div');
+}
+
+function fun2(event) {
+  // event.stopPropagation();
+  console.log('Mouse over textarea');
+}
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+#mouseover {
+  text-align: right;
+  background-color: purple;
+  width: 300px;
+  height: 300px;
+}
 </style>
